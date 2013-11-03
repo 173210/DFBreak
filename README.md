e13d22c0bda739357522f772bf8ed3afdbccb658
173210 <root.3.173210@live.com>
2013-11-03 18:15:06 +0900

    Version 1.1

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ DAYFILERのroot化ツールです。
 
 ## ビルド方法
 1. 通常のAndroidプロジェクトとしてコンパイルする
-2. DAYFILERの/system/appに存在するAlarmShutDownNotification.apkを使って偽装署名する
+2. DAYFILERの/system/appに存在するAlarmShutDownNotification.apkを使ってバグ9695860で偽装署名する
 
 ### 偽装署名の方法
 1. [こちら](https://github.com/Fuzion24/ZipArbitrage/tree/master/bin)よりAndroidMasterKeys.jarをダウンロードする
 2. 次のようにターミナルで入力し、実行する
-`java -jar AndroidMasterKeys.jarへのパス -a オリジナルのAPKのパス -z ビルドしたAPKのパス`
+`java -jar AndroidMasterKeys.jarへのパス -b -a オリジナルのAPKのパス -z ビルドしたAPKのパス`
 
 その後、偽装署名されたAPKが"MasterKeysModded-AlarmShutDownNotification.apk"という名前で出力されます。
--- src/com/gsl/szsp/shutdown/AlarmShutDownNotification.java
+++ src/com/gsl/szsp/shutdown/AlarmShutDownNotification.java
@@ -19,6 +19,7 @@
 package com.gsl.szsp.shutdown;
 
 import java.io.FileReader;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 
 import android.app.Activity;
@@ -38,8 +39,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AlarmShutDownNotification extends Activity implements OnClickListener, OnItemSelectedListener {
-	Spinner addr_spinner;
-	EditText addr_editText;
+	Spinner offset_spinner;
+	EditText offset_editText;
 	CheckBox verfying_checkBox;
 	Button exec_button;
 	Button reboot_button;
@@ -48,13 +49,13 @@ public class AlarmShutDownNotification extends Activity implements OnClickListen
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		int i;
-		String[] addr_spinner_entries = getResources().getStringArray(R.array.addr_spinner_entries);
+		String[] offset_spinner_entries = getResources().getStringArray(R.array.offset_spinner_entries);
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		addr_spinner = (Spinner)findViewById(R.id.addr_spinner);
-		addr_editText = (EditText)findViewById(R.id.addr_editText);
+		offset_spinner = (Spinner)findViewById(R.id.offset_spinner);
+		offset_editText = (EditText)findViewById(R.id.offset_editText);
 		verfying_checkBox = (CheckBox)findViewById(R.id.verifying_checkBox); 
 		exec_button = (Button)findViewById(R.id.exec_button);
 		reboot_button = (Button)findViewById(R.id.reboot_button);
@@ -62,7 +63,7 @@ public class AlarmShutDownNotification extends Activity implements OnClickListen
 		
 		out_textView.setMovementMethod(ScrollingMovementMethod.getInstance());
 
-		addr_spinner.setOnItemSelectedListener(this);
+		offset_spinner.setOnItemSelectedListener(this);
 		exec_button.setOnClickListener(this);
 		reboot_button.setOnClickListener(new OnClickListener() {
             @Override
@@ -72,56 +73,61 @@ public class AlarmShutDownNotification extends Activity implements OnClickListen
             }
         });
 		
-		for (i = 0; i < addr_spinner_entries.length - 1; i++) {
-			if (Build.DISPLAY.equals(addr_spinner_entries[i])) {
-				out_textView.append(getString(R.string.out_addr_found));
-				addr_spinner.setSelection(i);
+		for (i = 0; i < offset_spinner_entries.length - 1; i++) {
+			if (Build.DISPLAY.equals(offset_spinner_entries[i])) {
+				out_textView.append(getString(R.string.out_offset_found));
+				offset_spinner.setSelection(i);
 				return;
 			}
 		}
-		out_textView.append(getString(R.string.out_addr_notfound));
-		addr_spinner.setSelection(i);
+		out_textView.append(getString(R.string.out_offset_notfound));
+		offset_spinner.setSelection(i);
 	}
 
 	@Override
 	public void onClick(View v) {
-		int buf;
-		long addr;
-		
+		int buffer;
+		long offset;
+
+		FileReader orig_su;
+		InputStream insert_su;
 		RandomAccessFile dev;
-		FileReader su;
 		
 		exec_button.setEnabled(false);
 		try {
-			addr = Long.decode(addr_editText.getText().toString());
+			offset = Long.decode(offset_editText.getText().toString());
 			out_textView.append(getString(R.string.out_dev_opening));
 			dev = new RandomAccessFile("/dev/block/mmcblk0", "rw");
-				if (verfying_checkBox.isChecked()) {
-				out_textView.append(getString(R.string.out_su_opening));
-				su = new FileReader("/system/xbin/su");
+			if (verfying_checkBox.isChecked()) {
+				out_textView.append(getString(R.string.out_orig_su_opening));
+				orig_su = new FileReader("/system/xbin/su");
 				out_textView.append(getString(R.string.out_seeking));
-				dev.seek(addr);
+				dev.seek(offset);
 				out_textView.append(getString(R.string.out_verifying));
-				while ((buf = su.read()) != -1) {
-					if (buf != dev.read()) {
+				while ((buffer = orig_su.read()) != -1) {
+					if (buffer != dev.read()) {
 						out_textView.append(Html.fromHtml(
 							"<font color=\"red\">" +
 								getString(R.string.out_invalid) + "</font><br>"));
 						dev.close();
-						su.close();
+						orig_su.close();
 						exec_button.setEnabled(true);
 						return;
 					}
 				}
-				out_textView.append(getString(R.string.out_su_closing));
-				su.close();
+				out_textView.append(getString(R.string.out_orig_su_closing));
+				orig_su.close();
 			}
 			out_textView.append(getString(R.string.out_seeking));
-			dev.seek(addr);
+			dev.seek(offset);
+			out_textView.append(getString(R.string.out_insert_su_opening));
+			insert_su = getResources().getAssets().open("su");
 			out_textView.append(getString(R.string.out_writing));
-			dev.write(Binaries.su);
+			while ((buffer = insert_su.read()) != -1) dev.write(buffer);
 			out_textView.append(getString(R.string.out_dev_closing));
 			dev.close();
+			out_textView.append(getString(R.string.out_insert_su_closing));
+			insert_su.close();
 			out_textView.append(getString(R.string.out_complete));
 		} catch (Exception e) {
 			out_textView.append(Html.fromHtml(
@@ -134,20 +140,20 @@ public class AlarmShutDownNotification extends Activity implements OnClickListen
 
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		String addr_spinner_selected =
-				(String)addr_spinner.getSelectedItem();
+		String offset_spinner_selected =
+				(String)offset_spinner.getSelectedItem();
 		
-		if (addr_spinner_selected.equals(
-				getString(R.string.addr_spinner_entry_201301111156))) {
-			addr_editText.setText("0xD9CB8870");
-			addr_editText.setFocusable(false);
+		if (offset_spinner_selected.equals(
+				getString(R.string.offset_spinner_entry_201301111156))) {
+			offset_editText.setText("0xD9CB8870");
+			offset_editText.setFocusable(false);
 		}
-		else if (addr_spinner_selected.equals(
-				getString(R.string.addr_spinner_entry_201307221456))) {
-			addr_editText.setText("0xD9CC7BD0");
-			addr_editText.setFocusable(false);
+		else if (offset_spinner_selected.equals(
+				getString(R.string.offset_spinner_entry_201307221456))) {
+			offset_editText.setText("0xD9CC7BD0");
+			offset_editText.setFocusable(false);
 		}
-		else addr_editText.setFocusable(true);
+		else offset_editText.setFocusable(true);
 	}
 
 	@Override
